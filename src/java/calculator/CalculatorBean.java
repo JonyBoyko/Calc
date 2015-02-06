59630e8c5a6b9b8b9e2c971c35d4bde5f765a4f4
Ivan <boyko.ivan9@gmail.com>
2015-02-07 01:34:29 +0200

    Calculation
    simple test proj

---
+++ src/java/calculator/CalculatorBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Jony
+ */
+@ManagedBean
+@RequestScoped
+    public class CalculatorBean {
+    private int _param1;
+    private int _param2;
+    private String _result;
+
+    public CalculatorBean() {
+    }
+
+    //
+    public int getParam1() {
+        return _param1;
+    }
+
+    public void setParam1(int param1) {
+        _param1 = param1;
+    }
+
+    public int getParam2() {
+        return _param2;
+    }
+
+    public void setParam2(int param2) {
+        _param2 = param2;
+    }
+
+    public String getResult() {
+        return _result;
+    }
+    // 
+
+    public String add(){
+        _result = Integer.toString(_param1 + _param2);
+        return "result";
+    }
+
+    public String substract(){
+        _result = Integer.toString(_param1 - _param2);
+        return "";
+    }
+
+    public String multiply(){
+        _result = Integer.toString(_param1 * _param2);
+        return "";
+    }
+
+    public String divide(){
+        _result = _param2 == 0 ? "n/a" : Double.toString(_param1 / (double)_param2);
+        return "";
+    }
+
+}
